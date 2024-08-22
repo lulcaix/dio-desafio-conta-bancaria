3cf4e33b69a791fc888e5509d14792ac779eb481
Lucas Silva <dev.lucasmiranda@hotmail.com>
2024-08-22 10:53:08 +0100

    inicio da criação código fonte da ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da agência: ");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = sc.next();
+
+        System.out.println("Por favor, informe o seu nome: ");
+        nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o valor que deseja depositar: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponivel." );
     }
 }
